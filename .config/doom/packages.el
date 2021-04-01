e851f0af8018c09956b9cc7e88d40cc6ce1b89c4
Lukas Lundmark <lukas018.lundmark@gmail.com>
2021-04-01 15:50:53 +0200

    Updated emacs configurations

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -49,10 +49,118 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+
+;; DIRED RELATED CONFIGURATIONS
+(package! peep-dired)
+(package! dired-narrow)
+(package! dired-subtree)
+
+;; DISTROTUBE RECOMMENDED PACKAGES
+(package! dmenu)
+(package! elfeed)
+(package! emms)
+(package! emojify)
+(package! mastodon)
+(package! md4rd)
+(package! peep-dired)
+(package! rainbow-mode)
+(package! sublimity)
+(package! tldr)
+(package! wc-mode)
+(package! writeroom-mode)
+
+
+(package! bluetooth)
+(package! frog-jump-buffer)
+(package! neotree)
+
+(package! telega)
+
+(package! emacs-webkit
+  :recipe (:host github
+           :repo "akirakyle/emacs-webkit"
+           :files (:defaults "Makefile" "*.h" "*.c" "*.js" "*.css" "*.so")
+           :branch "main"))
+
+;; EXWM
+(package! helm-exwm)
+(package!  exwm)
+(package! ivy-posframe)
+
+;; DEVELOPMENT
+(package! exec-path-from-shell)
+(package! desktop-environment)
+(package! pyvenv)
+(package! vue-mode)
+(package! magit-todos)
+
+
+;; ORG-MODE-PACKAGES
+(package! org-bullets)
+(package! org-alert)
 (package! org-fragtog)
-(package! ewal)
-(package! ewal-spacemacs-themes)
-(package! ewal-evil-cursors)
-(package! spaceline)
-;; Aggresive tabbing
-(package! aggressive-indent)
+(package! org-ref)
+(package! org-re-reveal)
+(package! org-re-reveal-ref)
+(package! org-tree-slide)
+(package! helm-org-rifle)
+(package! org-recent-headings)
+(package! org-sticky-header)
+(package! org-super-agenda)
+(package! org-bookmark-heading)
+(package! org-protocol-capture-html
+  :recipe (:host github
+           :repo "alphapapa/org-protocol-capture-html"))
+(package! org-web-tools)
+(package! org-transclusion
+  :recipe (:host github
+           :repo "nobiot/org-transclusion"))
+
+;; Following the installation instructions on the github
+(package! org-roam-bibtex
+  :recipe (:host github :repo "org-roam/org-roam-bibtex"))
+;; When using org-roam via the `+roam` flag
+(unpin! org-roam company-org-roam)
+;; When using bibtex-completion via the `biblio` module
+(unpin! bibtex-completion helm-bibtex ivy-bibtex)
+
+
+;; STYLING
+(package! centered-window
+:recipe (:host github :repo "anler/centered-window-mode"))
+(package! hide-lines
+  :recipe (:host github :repo "vapniks/hide-lines"))
+
+(package! eaf
+  :recipe (:host github
+           :files (:defaults "*.el" "app" "core" "eaf.py")
+           :repo "manateelazycat/emacs-application-framework"))
+
+;; Creating
+(package! org-chef
+  :recipe (:host github
+          :repo "chobbes/org-chef"))
+(package! org-ql)
+(package! org-ml)
+
+
+(package! exwm-firefox-core)
+(package! exwm-firefox-evil)
+
+(package! nano-emacs
+  :recipe (:host github
+           :repo "rougier/nano-emacs"
+           :files (:defaults "nano-writer.el")))
+
+(package! cython-mode)
+(package! flycheck-cython)
+
+
+(package! sphinx-doc
+  :recipe (:host github
+           :repo "naiquevin/sphinx-doc.el"))
+
+(package! nano-doom
+  :recipe (:host
+           github :repo "skyler544/doom-nano-testing"))
